31495ad4ea7ec7d3658322044a25c2ad6999105f
escJunior <devescjunior@gmail.com>
2022-10-13 01:27:55 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './app.css';
+import Videolist from './components/video_list/video_list';
 
 function App() {
-  return <h1>Hello :)</h1>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    console.log('useEffect');
+
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCLBkyHzYHdu_gesJwj4eCQuviMB0P9t9I',
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log('error', error));
+  }, []);
+  return <Videolist videos={videos} />;
 }
 
 export default App;
